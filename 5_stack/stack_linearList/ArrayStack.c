a534af3e790bef9982ceb5d29987db4061031f68
hemingwayInTheX <leeht620@gmail.com>
2021-04-07 00:54:43 +0900

    implement theory and concept of linearList-based <Stack>

---
+++ 5_stack/stack_linearList/ArrayStack.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayStack.h"
+
+// 공백스택 생성, create a empty stack
+Stack* create() {
+	
+	// Fill your code
+	Stack* S = (Stack*)malloc(sizeof(Stack));
+	S->top = -1;//공백상태의 stack에서 top이 가리키고 있는 데이터가 없기 때문에 -1로 초기화
+	return S;
+}
+
+// 스택이 포화상태인지 확인하는 연산, operations to determine if the stack is full
+int isFull(Stack* S) {
+
+	// Fill your code
+	return S->top == MAX_SIZE - 1;
+}
+
+// 스택이 공백상태인지 확인하는 연산, operations to determine if the stack is empty			
+int isEmpty(Stack* S) {
+
+	// Fill your code
+	return S->top == -1;//top이 -1을 가리킨다는건 데이터가 없다는 의미
+}
+
+// 스택의 top에 요소를 추가하는 연산, operations to add elements to the top of a stack
+void push(Stack* S, element x) {
+
+	// Fill your code
+	if (isFull(S)) {//포화상태일 경우 더 이상 데이터를 삽입할 수 없음
+		printf("[ERROR] Stack is FULL\n");
+		return ERROR;
+	}
+	else {
+		/*S->top += 1;
+		S->stack[S->top] = x;*/
+		S->stack[++(S->top)] = x;//전위연산자 --> 먼저 값 증가시키고 다음 값 할당
+	}
+
+}
+
+// 스택에서 마지막에 저장된 요소 삭제 및 반환하는 연산, operations that delete and return the last stored element in the stack	
+element pop(Stack* S) {
+
+	// Fill your code
+	if (isEmpty(S)) {
+		printf("[ERROR] Stack is EMPTY!!\n");
+		return ERROR;
+	}
+	else {
+		/*element e = S->stack[S->top];
+		S->top -= 1;*/
+	return S->stack[(S->top)--];//후위연산자-->먼저 값을 반환한 다음에 TOP감소
+	}
+}
+
+// 스택에서 마지막에 저장된 요소를 반환하는 연산, operations that return the last stored element in the stack
+element peek(Stack* S) {
+
+	// Fill your code
+	if (isEmpty(S)) {
+		printf("[ERROR] Stack is EMPTY!!\n");
+		return ERROR;
+	}
+	else {
+		return S->stack[S->top];
+	}
+
+}
+
+// 스택의 모든 요소 출력, display all elements in the stack
+void displayStack(Stack* S) {
+	int i;
+	printf("STACK [ "); 
+	for(i=0; i<=S->top; i++)
+		printf("%d ", S->stack[i]);
+	printf(" ]\ttop: %d\n", S->top);
+}
+
+// 스택 비우기, empty stack
+void clear(Stack* S) {
+	
+	// Fill your code
+	while()
+
+}				
+
---
+++ 5_stack/stack_linearList/ArrayStackMain.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "ArrayStack.h"
+
+int main() {
+	Stack* S;
+	element item;
+	int i;
+	
+	printf("\n(1) 공백 스택 생성하기\n");	// (1) To create a blank stack
+
+	// Fill your code
+	S = create();
+	displayStack(S);
+	
+	printf("\n(2) 스택에 10, 20을 차례로 삽입\n");	// (2) Insert 10, 20 into the stack in order
+
+	// Fill your code
+	push(S, 10);
+	displayStack(S);
+	push(S, 20);
+	displayStack(S);
+
+	printf("\n(3) 현재 top의 요소를 출력\n");	// (3) Display elements of the current top
+
+	// Fill your code	// Elements stored in top: 
+	element topElement = peek(S);
+	printf("top에 저장된 요소 : %d\n", topElement);
+
+	printf("\n(4) 스택에서 요소 삭제\n");	// (4) Deleting an element from the stack
+	
+	// Fill your code	// elimination
+	element eliminationData1 = pop(S);
+	printf("%d 삭제\n", eliminationData1);
+	displayStack(S);
+	
+	printf("\n(5) 스택에서 요소 삭제\n");	// (5) Deleting an element from the stack
+	
+	// Fill your code	// eliminaion
+	element eliminationData2 = pop(S);
+	printf("%d 삭제\n", eliminationData2);
+	displayStack(S);
+
+	printf("\n(6) 스택에서 요소 삭제\n");	// (6) Deleting an elment from the stack
+	
+	// Fill your code
+	element eliminationData3 = pop(S);
+	//printf("%d 삭제\n", eliminationData3);
+	displayStack(S);
+
+	printf("\n(7) 스택에 1부터 10까지 자연수를 차례로 삽입\n");	// (7) Insert natural numbers from 1 to 10 into the stack in oerder
+	
+	// Fill your code
+	for (int i = 0; i < 11; i++){
+		push(S, i);
+	}
+	displayStack(S);
+
+	printf("\n(8) 스택 비우기\n");	// (8) Empty stack
+	
+	// Fill your code
+	clear(S);
+	displayStack(S);
+
+	return 0;
+}
