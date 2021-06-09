bd0147fdf5a66292d8a83c785ab26f48a656d202
hemingwayInTheX <leeht620@gmail.com>
2021-06-09 19:28:46 +0900

    completed implementation of 3<Algorithms> and path output using <STACK> operations

--- 14_shortestPath_Algorithm/ArrayGraph.c
+++ 14_shortestPath_Algorithm/ArrayGraph.c
@@ -110,7 +110,6 @@ void dijkstra(Graph* G, int v) {
 	저렴할 때 해당 값으로 갱신
 	3) 모든 정점에 대한 relaxation이 끝나면 작업 종료*/
 	int u = 0;//추가되는 정점에 대한 정보를 가지고 있는 변수
-	G->S[0] = v;//집합 S에 시작정점에 대한 정보를 기억하도록함
 	
 	for (int i = 0; i < G->n - 1; i++){//dist, pred 초기화 작업
 		G->dist[i] = G->cost[v][i];//dist의 초기값은 인접행렬에서 시작정점의 행
@@ -120,19 +119,20 @@ void dijkstra(Graph* G, int v) {
 		else {
 			G->pred[i] = NULL;
 		}
+	G->S[v] = 1;//집합 S에 시작정점에 대한 정보를 기억하도록함
 	}/*LOOP_1*/
-	for (int i = 0; G->S[i+1]!=G->n; i++){//모든 정점에 대한 relaxation 과정을 완료할 때까지 루프 진행
+	for (int i = 0; i < G->n; i++){//모든 정점에 대한 relaxation 과정을 완료할 때까지 루프 진행
 		u = nextVertex(G);//dist에서 최소비용이 가장 작은 정점을 선택
-		G->S[i] = u;//집합 S에 추가 --> 정점을 추가함으로써 해당 정점을 지나칠 경우 최소비용이 갱신되는지 확인
-		for (int w = 0; w < G->n ; w++){
-			if (G->S[i + 1] != w && G->dist[u] + G->cost[u][w]) {
+		G->S[u] = 1;//집합 S에 추가 --> 정점을 추가함으로써 해당 정점을 지나칠 경우 최소비용이 갱신되는지 확인
+		for (int w = 0; w < G->n ; w++){//타겟 정점 갱신
+			if (G->S[w]!=1 && G->dist[u] + G->cost[u][w]< G->dist[w]) {//기존 비용이 relaxation결과보다 비싸면 해당 값으로 갱신
 				G->dist[w] = G->dist[u] + G->cost[u][w];
 				G->pred[w] = u;
 			}
 		}
 	}/*LOOP_2*/
 }
-
+//G->S[i + 1] != w
 // Bellman-Ford Algorithm
 int bellmanFord(Graph* G, int v) {
 
@@ -144,6 +144,7 @@ int bellmanFord(Graph* G, int v) {
 	3) 정점의 개수와 동일한 횟수에서도 최단 경로값이 갱신된다면
 	해당 그래프는 음수 사이클을 지니고 있음*/
 	int numOfGraph = G->n;//현재 그래프에 삽입된 노드의 개수
+	int w = 0;
 	int u = 0;
 	for (int i = 0; i < numOfGraph; i++) {//모든 정점에 대한 최소 비용을 무한대로 초기화
 		G->dist[i] = INF;
@@ -151,13 +152,25 @@ int bellmanFord(Graph* G, int v) {
 	G->dist[v] = 0;//시작 정점은 0으로 초기화
 
 	for (int i = 0; i < numOfGraph - 1; i++) {//정점의 개수보다 1적은 만큼 루프 수행
-		for () {
-			if (G->dist[v] > G->dist[u] + G->cost[u][v]) {//relaxation 결과가 기존의 경로보다 저렴하면 해당 결과값으로 갱신
-				G->dist[v] = G->dist[u] + G->cost[u][v];
+		for (int u = 0; u < numOfGraph; u++) {//내부 중첩 for문 = 모든 간선에 대한 relaxation을 위한 루프
+			for (int w = 0; w < numOfGraph; w++){
+				if (G->cost[u][w]== 0 || G->cost[u][w] == INF) {//무한대 OR 0 을 원소로 가지면 루프 재수행
+					continue;
+				}
+				if (G->dist[w] > G->dist[u] + G->cost[u][w]) {//갱신된 비용이 기존의 값보다 저렴하면 해당 값으로 갱신
+					G->dist[w] = G->dist[u] + G->cost[u][w];
+					G->pred[w] = u;
+				}
 			}
 		}
-	}
-
+	}/*LOOP_1*/
+	for (int u = 0; u < numOfGraph; u++) {//정점-1만큼 relaxation 수행 후 다시 한 번 검사 --> 음수 사이클 여부 chk
+		for (int w = 0; w < numOfGraph; w++) {
+			if (G->dist[w] > G->dist[u] + G->cost[u][w]) {//dist값이 갱신되면 false 반환
+				return FALSE;
+			}
+		}
+	}/*LOOP_2*/
 }	
 
 // Floyd-Warshall Algorithm
@@ -188,12 +201,30 @@ void floyd(Graph* G){
 
 // 최단경로 출력, display shortest path
 void printShortestPath(Graph* G, int src, int dst) {
-	printf("%c -> %c 최단 경로: %c", src+65, dst+65, src+65);	// "%c -> %c Shortest Path: %c"
-	int v = dst;
+	printf("%c -> %c 최단 경로: %c\t", src+65, dst+65, src+65);	// "%c -> %c Shortest Path: %c"
+	int v = dst;//v=3
 	Stack* stack = createStack();
 
 	// Fill your code
-	
+
+	/*===스택을 이용한 PRED배열 출력===
+	종료정점을 거쳐간 정점을 역순으로 추적 --> 마지막에 스택에 PUSH되는 요소가 시작정점
+	(시작정점은 사전에 출력되므로 그 다음 정점부터 출력하도록)*/
+	int temp = G->pred[v];//비교연산을 위한 변수 
+	element item = 0;
+
+	push(stack, v);//종료 정점 먼저 스택에 저장
+	for (int i = 0; i < G->n; i++) {
+		if (temp == 0)break;//시작정점이면 루프 종료
+		if (temp == i) {//인덱스 i가 종료정점의 이전 정점 --> 해당 인덱스 stack 연산 수행 및 temp 갱신
+			push(stack, i);
+			temp = G->pred[i];
+		}
+	}
+	while (stack->top != -1) {//스택이 공백이면 작업 종료
+		item = pop(stack);
+		printf("%c\t", item + 65);
+	}
 
 	printf("\n");
 	clearStack(stack);
